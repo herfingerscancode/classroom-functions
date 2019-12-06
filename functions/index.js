f93d854a86a4b72655b578d0fdb58d48733b50be
joshua <mama27j@gmail.com>
2019-12-06 18:52:39 +0300

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+// Create and Deploy Your First BFast Function
+
+
+// exports.helloWorld = {
+//     onRequest: (request, response) => {
+//         response.send("Hello from BFast!");
+//     }
+// };
